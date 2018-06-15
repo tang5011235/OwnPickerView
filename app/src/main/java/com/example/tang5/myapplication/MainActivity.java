874ec583a7251af54955a476c4d7994a6549f13f
thf <tang5011235@163.com>
2018-06-15 12:17:50 +0800

    sdf

--- app/src/main/java/com/example/tang5/myapplication/MainActivity.java
+++ app/src/main/java/com/example/tang5/myapplication/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.tang5.myapplication;
 
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -25,39 +21,13 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ButterKnife.bind(this);
-
-
-		View inflate = LayoutInflater.from(this).inflate(R.layout.view_four, mFlContainer, false);
-		FourParametersTimeWheelView wheelView = new FourParametersTimeWheelView(inflate);
-		wheelView.setDateListener(new FourParametersTimeWheelView.DateListener() {
+		final PeriodTimeWheelView wheelView = new PeriodTimeWheelView();
+		mTvDate.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onDateSelected(int year, int month, int day, String period) {
-				mTvDate.setText(new StringBuilder()
-						.append(year)
-						.append("-")
-						.append(month)
-						.append("-")
-						.append(day)
-						.append(" ")
-						.append(period)
-						.toString()
-				);
+			public void onClick(View view) {
+				wheelView.show(getSupportFragmentManager(),"1");
 			}
 		});
-		wheelView.setDate();
-
-
-		AlertDialog dialog = new AlertDialog.Builder(this,R.style.MyDialog)
-				.setView(inflate)
-				.create();
-		dialog.getWindow()
-				.setGravity(Gravity.BOTTOM);
-		View decorView = dialog.getWindow().getDecorView();
-		decorView.setPadding(0,0,0,0);
-		ViewGroup.LayoutParams layoutParams = dialog.getWindow().getAttributes();
-		layoutParams.width= ViewGroup.LayoutParams.MATCH_PARENT;
-		layoutParams.height= ViewGroup.LayoutParams.WRAP_CONTENT;
-		dialog.show();
 
 	}
 
--- app/src/main/java/com/example/tang5/myapplication/PeriodTimeWheelView.java
+++ app/src/main/java/com/example/tang5/myapplication/PeriodTimeWheelView.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * @author：thf on 2018/6/14 0014 17:13
@@ -24,12 +26,41 @@ public class PeriodTimeWheelView extends DialogFragment {
 
 	private View mRootView;
 
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setStyle(DialogFragment.STYLE_NO_FRAME, R.style.MyDialog);
+	}
+
 	@NonNull
 	@Override
 	public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		 mRootView = LayoutInflater.from(getContext()).inflate(R.layout.view_four, (ViewGroup) (getActivity().findViewById(android.R.id.content)), false);
-		builder.setView(mRootView);
-		return builder.create();
+
+		mRootView = LayoutInflater.from(getContext()).inflate(R.layout.view_four, (ViewGroup) (getActivity().findViewById(android.R.id.content)), false);
+		FourParametersTimeWheelView wheelView = new FourParametersTimeWheelView(mRootView);
+		wheelView.setDate();
+
+		Dialog dialog = new Dialog(getActivity(),R.style.MyDialog);
+
+		Window window = dialog.getWindow();
+		WindowManager.LayoutParams wlp = window.getAttributes();
+		wlp.gravity = Gravity.BOTTOM;
+		window.setAttributes(wlp);
+		dialog.getWindow()
+				.setGravity(Gravity.BOTTOM);
+		View decorView = dialog.getWindow().getDecorView();
+		decorView.setPadding(0,0,0,0);
+
+
+		dialog.setContentView(mRootView);
+
+
+		return dialog;
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 	}
 }
